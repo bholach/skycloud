8e0e01627bb9dd34be91ba231553e1b627d0f271
bhola kumar <33161752+bholach@users.noreply.github.com>
2018-12-06 16:40:18 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
-const dbURL = "mongodb://scloud:scloud@ds241737.mlab.com:41737/skycloud/";
+const dbURL = "mongodb://tejash:skycloud4$@ds241737.mlab.com:38888/skycloud/";
+//mongodb://<dbuser>:<dbpassword>@ds038888.mlab.com:38888/skycloud
 module.exports = {
     //database: 'mongodb://brad:brad@ds121190.mlab.com:21190/meanauthapp',   //prod
     database: dbURL,    //dev
